44d9c717b572850802a9d8b83878f8db3d67daf3
hackerrank <me@hackerrank.com>
2022-09-29 21:47:24 -0400

    final changes to code

---
+++ src/lib/firebaseConfig.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+    apiKey: "AIzaSyCrGtgzi6p0HqeHFyq8uAKRvyum6jVTtEA",
+    authDomain: "svelte-todo-app-b9e11.firebaseapp.com",
+    projectId: "svelte-todo-app-b9e11",
+    storageBucket: "svelte-todo-app-b9e11.appspot.com",
+    messagingSenderId: "357717993884",
+    appId: "1:357717993884:web:1be02cb87973ecc7c04059",
+    measurementId: "G-GYD7NXCX05"
+};
