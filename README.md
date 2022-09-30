736e3a613619671e63337319d079b1653c1ec744
hackerrank <me@hackerrank.com>
2022-09-30 17:00:47 -0400

    comments and readme

--- README.md
+++ README.md
@@ -1,38 +1,20 @@
-# create-svelte
+# CS279 Tier 1 HW: Svelte and Firebase
 
-Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
+This code creates a basic to-do list web app using Svelte and Firebase. In addition to the basic functionality of adding and checking off todos, it also allows users to delete todos entirely.
 
-## Creating a project
+## Usage
 
-If you're seeing this, you've probably already done this step. Congrats!
+Clone this repository (from the command line or using GitHub Desktop). If from the command line, use the following command:
 
-```bash
-# create a new project in the current directory
-npm create svelte@latest
+git clone https://github.com/19katz/svelte-todo-app.git
 
-# create a new project in my-app
-npm create svelte@latest my-app
-```
+Once cloned locally, open up the to-do app by running the following commands:
+* `npm i firebase`
+* `npm run dev`
 
-## Developing
+Then, navigate to the localhost link given on the terminal console. Ensure that Firebase is installed before going to the link, or it will not work.
 
-Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+## Citations
 
-```bash
-npm run dev
+This code was created using [this tutorial playlist](https://www.youtube.com/playlist?list=PLm_Qt4aKpfKiGbdjaHdOpry6Neza0etxZ)
 
-# or start the server and open the app in a new browser tab
-npm run dev -- --open
-```
-
-## Building
-
-To create a production version of your app:
-
-```bash
-npm run build
-```
-
-You can preview the production build with `npm run preview`.
-
-> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,14 +3,25 @@
     import { getFirestore, collection, onSnapshot, doc, addDoc, updateDoc, deleteDoc } from "firebase/firestore";
     import { firebaseConfig } from "$lib/firebaseConfig";
     
+    // Initialize Firebase and get the corresponding database from Firestore.
     const firebaseApp = initializeApp(firebaseConfig);
     const db = getFirestore();
+    // Get the todos collection from the database.
     const colRef = collection(db, "todos");
     console.log(db);
 
+    // Stores todos locally. This is used to update the website, and
+    // it updates with the data from Firebase.
     let todos = [
     ];
 
+    // Unsubscribes to the store
+    // onSnapshot serves the purpose of being a listener, which will be used
+    // to update the data on the website.
+    // In this case, we look at each document in the query snapshot, which represents
+    // a todo, and add it to a list of Firebase todos (fbTodos).
+    // This list of todos is then set to our internal variable for the website,
+    // todos, which is used to update what is displayed.
     const unsubscribe = onSnapshot(colRef, (querySnapshot) => {
         let fbTodos = [];
         querySnapshot.forEach((doc) => {
@@ -23,6 +34,9 @@
 
     let task = "";
 
+    // Function to add a new todo to the list by adding it to Firestore.
+    // Uses addDoc (which adds todos with a default id to the database)
+    // whenever a new todo is made by the user. 
     const addTodo = async () => {
         if (task != "") {
             const docRef = await addDoc(collection(db, "todos"), {
@@ -34,16 +48,21 @@
         task = "";
     };
 
+    // Uses updateDoc, which updates a field of the database entry/document
+    // to update the completion status of a todo by toggling it.
     const markTodoComplete = async (item) => {
         await updateDoc(doc(db, "todos", item.id), {
             isComplete: !item.isComplete
         });
     }
 
+    // Uses deleteDoc, which deletes a document by its id from the database.
     const deleteTodo = async (id) => {
         await deleteDoc(doc(db, "todos", id));
     }
 
+    // When the enter key is pressed, add the todo using the addTodo function
+    // defined above.
     const keyIsPressed = (event) => {
         if (event.key == "Enter") {
             addTodo();
@@ -53,16 +72,23 @@
     $: console.table(todos);
 </script>
 
+<!--Adds html code for the area to submit a new task.-->
 <input type="text" placeholder="Add a task" bind:value={task} />
 <button on:click={addTodo}>Add</button>
 
 <ol>
+    <!--Loop over all the todos in the todo list that has been updated from Firestore.-->
     {#each todos as todo}
+        <!--Use the isComplete field of the todo to show its class-->
         <li class:complete={todo.isComplete}>
             <span>
+                <!--Display the text of the task-->
                 {todo.task}
             </span>
             <span>
+                <!--Define buttons for marking a todo as finished and deleting it.
+                When the check is clicked, call markTodoComplete. When the x is clicked,
+                call deleteTodo.-->
                 <button on:click={() => markTodoComplete(todo)}>✔</button>
                 <button on:click={() => deleteTodo(todo.id)}>⨉</button>
             </span>
@@ -70,8 +96,10 @@
     {/each}
 </ol>
 
+<!--When a key is held down, call keyIsPressed to check if it's the enter key.-->
 <svelte:window on:keydown={keyIsPressed} />
 
+<!--Styling for the "complete" class of each list item.-->
 <style>
     .complete {
         text-decoration: line-through;
